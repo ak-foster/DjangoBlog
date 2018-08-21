8fa3576b6f8e96b4e3a76d942f4e03093834a88e
Andrew Foster <akfoster@uw.edu>
2018-08-20 20:48:08 -0700

    Added REST framework and User, Groups endpoints.

--- mysite/myblog/urls.py
+++ mysite/myblog/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 
 from .views import detail_view, list_view
+from django.conf.urls import url, include
+from rest_framework import routers
+from myblog import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', list_view, name="blog_index"),
     path('posts/<int:post_id>/', detail_view, name="blog_detail"),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -2,12 +2,17 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.template import loader
 from myblog.models import Post
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from myblog.serializers import UserSerializer, GroupSerializer
+
 
 def list_view(request):
     published = Post.objects.exclude(published_date__exact=None)
     posts = published.order_by('-published_date')
     context = {'posts': posts}
     return render(request, 'list.html', context) # HttpResponse("Hello, world.")
+
 
 def detail_view(request, post_id):
     published = Post.objects.exclude(published_date__exact=None)
@@ -18,3 +23,18 @@ def detail_view(request, post_id):
     context = {'post': post}
     return render(request, 'detail.html', context)
 
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
